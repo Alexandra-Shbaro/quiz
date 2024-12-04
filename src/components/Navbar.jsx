82a17922811c18b7540d2d8b22c623bf93d39f5b
Alexandra Shbaro <alexandrashbaro@gmail.com>
2024-12-04 22:27:29 +0200

    added navigation of popups to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../redux/userSlice';
+import LoginPopup from './LoginPopup';
+import SignupPopup from './SignupPopup';
 
 const Navbar = () => {
   const dispatch = useDispatch();
   const { isLoggedIn, username, totalPoints } = useSelector((state) => state.user);
+
+  const [showLoginPopup, setShowLoginPopup] = useState(false);
+  const [showSignupPopup, setShowSignupPopup] = useState(false);
 
   return (
     <nav className="navbar">
@@ -17,11 +22,15 @@ const Navbar = () => {
           </>
         ) : (
           <>
-            <button>Login</button>
-            <button>Signup</button>
+            <button onClick={() => setShowLoginPopup(true)}>Login</button>
+            <button onClick={() => setShowSignupPopup(true)}>Signup</button>
           </>
         )}
       </div>
+
+      {showLoginPopup && <LoginPopup onClose={() => setShowLoginPopup(false)} />}
+
+      {showSignupPopup && <SignupPopup onClose={() => setShowSignupPopup(false)} />}
     </nav>
   );
 };
