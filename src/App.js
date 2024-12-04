c6f178112e49f0806f0c9863ce4c297c660a07b1
Alexandra Shbaro <alexandrashbaro@gmail.com>
2024-12-04 22:54:18 +0200

    adjusted app routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import QuizDetailPage from './pages/QuizDetailPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import QuizDetailPage from './pages/QuizDetailPage';
 
 const App = () => {
   return (
     <Router>
-      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/quiz/:id" element={<QuizDetailPage />} />
