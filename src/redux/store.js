d2c046a8aae23b3b9033a322f49bf9347aceb4d3
Alexandra Shbaro <alexandrashbaro@gmail.com>
2024-12-04 20:31:27 +0200

    added redux store

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import quizReducer from './quizSlice';
+import userReducer from './userSlice';
+
+export const store =configureStore({
+    reducer: {
+        quiz: quizReducer,
+        user: userReducer,
+    },
+});
