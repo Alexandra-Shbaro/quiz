acb153ddcca56dbb619e610288f794e571415a16
Alexandra Shbaro <alexandrashbaro@gmail.com>
2024-12-04 20:54:38 +0200

    added quiz list

---
+++ src/components/QuizList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import QuizCard from './QuizCard';
+
+const QuizList = ({ quizzes, onQuizClick }) => (
+  <div className="quiz-list">
+    {quizzes.map((quiz) => (
+      <QuizCard key={quiz.id} quiz={quiz} onClick={() => onQuizClick(quiz)} />
+    ))}
+  </div>
+);
+
+export default QuizList;
