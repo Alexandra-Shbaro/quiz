aba6697a7caaa34247c520cde797cce8b9506241
Alexandra Shbaro <alexandrashbaro@gmail.com>
2024-12-04 20:49:29 +0200

    added user slice

---
+++ src/redux/userSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isLoggedIn: false,
+  totalPoints: 0,
+  username: null,
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.isLoggedIn = true;
+      state.username = action.payload.username;
+    },
+    logout: (state) => {
+      state.isLoggedIn = false;
+      state.username = null;
+      state.totalPoints = 0;
+    },
+    addPoints: (state, action) => {
+      state.totalPoints += action.payload;
+    },
+  },
+});
+
+export const { login, logout, addPoints } = userSlice.actions;
+export default userSlice.reducer;
