f81d24a0a8c7d88426df6542adda3319bba33dec
Alexandra Shbaro <alexandrashbaro@gmail.com>
2024-12-04 22:26:15 +0200

    added password to login popup

--- src/components/LoginPopup.jsx
+++ src/components/LoginPopup.jsx
@@ -1,31 +1,35 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { login } from '../redux/userSlice';
-
 const LoginPopup = ({ onClose }) => {
-  const [username, setUsername] = useState('');
-  const dispatch = useDispatch();
-
-  const handleLogin = () => {
-    if (username.trim()) {
-      dispatch(login({ username }));
-      onClose();
-    }
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const dispatch = useDispatch();
+  
+    const handleLogin = () => {
+      if (username.trim() && password.trim()) {
+        dispatch(login({ username, password }));
+        onClose();
+      }
+    };
+  
+    return (
+      <div className="popup">
+        <h2>Login</h2>
+        <input 
+          type="text" 
+          placeholder="Enter username" 
+          value={username} 
+          onChange={(e) => setUsername(e.target.value)} 
+        />
+        <input 
+          type="password" 
+          placeholder="Enter password" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+        />
+        <button onClick={handleLogin}>Login</button>
+        <button onClick={onClose}>Close</button>
+      </div>
+    );
   };
-
-  return (
-    <div className="popup">
-      <h2>Login</h2>
-      <input 
-        type="text" 
-        placeholder="Enter username" 
-        value={username} 
-        onChange={(e) => setUsername(e.target.value)} 
-      />
-      <button onClick={handleLogin}>Login</button>
-      <button onClick={onClose}>Close</button>
-    </div>
-  );
-};
-
-export default LoginPopup;
+  
+  export default LoginPopup;
+  
